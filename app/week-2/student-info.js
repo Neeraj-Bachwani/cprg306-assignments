c7e4814e728a7ad1818ec1310d2ba2aaa6fd3f0d
Neeraj-Bachwani <NeerajPradeepkumar.Bachwani@edu.sait.ca>
2025-01-15 17:43:56 -0700

    Assignment 1 Finished

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link  from "next/link";  
+
+export default function StudentInfo() { 
+    
+    return (
+        <div>
+            <h1>Neeraj Bachwani</h1>
+            <Link href="https://github.com/Neeraj-Bachwani?tab=repositories"> Neeraj Bachwani Github Repo </Link>
+        </div>
+    );
+
+
+}
